5152c57f27f6b2fde042ee3a6dfa235b6eef935a
GioGajishvili <giorgi.gajishvili.1@btu.edu.ge>
2022-06-11 00:21:01 +0400

    fixed errors

--- app.py
+++ app.py
@@ -1,49 +1,66 @@
 from flask import Flask, request, render_template, url_for, redirect, session
-app = Flask(__name__, template_folder="template")
 from datetime import datetime, timedelta
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__, template_folder="template")
 
 app.secret_key = "Gajexa"
-app.permanent_session_lifetime = timedelta(seconds=10)
+app.permanent_session_lifetime = timedelta(seconds=40)
 
-# class model :
-#     id = db.Column('user_id', db.Integer, primary_key=True)
-#     name = db.Column(db.String(30))
-#     email = db.Column(db.String(40))
-#     password = db.Column(db.String(30))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///RegisterDatabase.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class RegisterDatabase (db.Model) :
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    userName = db.Column(db.String(30))
+    email = db.Column(db.String(40))
+    password = db.Column(db.String(30))
+
+db.create_all()
 
 @app.route('/registration', methods=['POST', 'GET'])
 def registraion():
     if request.method == 'POST':
-        # user = request.form["nameUser"]
-        # request.form ["emailUser"]
-        # request.form ["passwordUser"]
-        # user_info = model (name = name, email = email, password = password)
-        # db.session.add(user_info)
-        # db.session.commit()
+        userName = request.form["nameUser"]
+        email = request.form ["emailUser"]
+        password = request.form ["passwordUser"]
+        user_info = RegisterDatabase (userName = userName, email = email, password = password)
+        db.session.add(user_info)
+        db.session.commit()
+
         return redirect(url_for('login'))
     else:
         return render_template('registration.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 @app.route('/login',  methods=['POST', 'GET'])
-def login():
+def login_validation():
     if request.method == 'POST':
         email = request.form["emailUser"]
         password = request.form["passwordUser"]
-        session["client"] = email, password
-        return redirect(url_for("mainPage"))
+        user = RegisterDatabase.query.filter_by(email=email,password = password).first()
+        session["client"] = email
+        try:
+            if user.password == password and user.email == email:
+                return redirect(url_for("mainPage"))
+        except:
+            return render_template("login.html")
+
     else:
         if "client" in session:
             return redirect(url_for('mainPage'))
-
-    return render_template("login.html")
 
 
 
 @app.route("/main_page")
 def mainPage():
-
-
-    return render_template('mainPage.html')
+    if "client" in session:
+        return render_template('mainPage.html')
+    else:
+        return redirect(url_for("login"))
